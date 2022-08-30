6c47597ef18f2fa94267d2651ec535d95b5b50b0
briomex <briomex@gmail.com>
2022-08-30 12:22:53 -0500

    Introduction to Django completed

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,7 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello World")
+# def index(request):
+#     """[polls/views/index]
+
+#     Args:
+#         request ([HTTP]): [Request]
+
+#     Returns:
+#         [Render]: [Request, Url, Dict(Question.objects.all())]
+#     """
+#     latest_question_list = Question.objects.all()
+#     return render(request, "polls/index.html", {
+#         "latest_question_list": latest_question_list
+#     })
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html",{
+#         "question": question
+#     })
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html",{
+#         "question":question
+#     })
+class ResultsView(DetailView):
+    template_name= "polls/results.html"
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html",{
+            "question":question,
+            "error_message": "You didn't selected a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        #return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
+        return redirect("polls:results",question_id)
--- premiosapp/premiosapp/settings.py
+++ premiosapp/premiosapp/settings.py
@@ -30,14 +30,33 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+"""INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+]"""
+DJANGO_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+THIRD_APPS = (
+    'django_extensions',
+)
+
+LOCAL_APPS = (
+    'polls.apps.PollsConfig',
+)
+
+
+INSTALLED_APPS = DJANGO_APPS  + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -105,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
